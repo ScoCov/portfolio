6ac8974795a42a2a12ed4c971fdd1c73d0dd747d
ScoCov <153954098+ScoCov@users.noreply.github.com>
2023-12-21 12:16:37 -0700

    Delete src directory

--- src/app/resume/interfaces/education.ts
+++
@@ -1,7 +0,0 @@
-export interface Education {
-  institution: string,
-  degree_title: string,
-  degree_major?: string,
-  institution_location?: string,
-  gpa: number
-}
--- src/app/resume/interfaces/workexperience.ts
+++
@@ -1,8 +0,0 @@
-export interface Workexperience {
-  company_name?: string,
-  company_location?: string,
-  start_date?: string,
-  end_date?: string,
-  job_title?: string,
-  duties: string[]
-}
